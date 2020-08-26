b89eac0f5831766a140054d1a919cae48e68b709
renatolacerda <renatolacerda@matur.com.br>
2020-08-26 10:47:09 -0300

    projeto completo

---
+++ ModelosBH/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ModelosBH.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
